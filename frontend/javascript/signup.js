b0043b04a4b4b3ef88340fcd72077152d19f983e
jorgie04 <jorgeedralev@gmail.com>
2023-10-10 22:40:12 +0800

    d pa tapos modal

--- frontend/javascript/signup.js
+++ frontend/javascript/signup.js
@@ -1,18 +1,33 @@
-function checkFields() {
-    var user = document.getElementById("username").value
-    var password = document.getElementById("password").value
-    var confirm = document.getElementById("confirm").value
 
-    if (user != "" || password != "" || confirm != "") {
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value; 
+    confirmPass = document.getElementById("confirm").value; 
+
+    if (username == '')
+        alert("Please enter username");
+    // If password not entered 
+    else if (password == '') 
+        alert ("Please enter Password"); 
+          
+    // If confirm password not entered 
+    else if (confirmPass == '') 
+        alert ("Please enter confirm password"); 
+          
+    // If Not same return False.     
+    else if (password != confirmPass) { 
+        alert ("\nPassword did not match: Please try again...") 
+        return false; 
+    } 
+
+    // If same return True. 
+    else{ 
         // Get the modal
         var modal = document.getElementById("popup");
-    
-        // Get the button that opens the modal
-        var btn = document.getElementById("btn-popup");
     
         // When the user clicks the button, open the modal 
         btn.onclick = function() {
             modal.style.display = "block";
         }
-    }
-}
+        document.getElementById("btn-popup").addEventListener("click", checkFields);
+        return true; 
+    } 
