f6e4b98342cfb43111aa5b9feb421a00c4186d0d
Justin Marley Fontanilla <71302306+jfontz@users.noreply.github.com>
2023-10-23 01:04:31 +0800

    Added JS to sidebar

--- app/static/javascript/sidebar.js
+++ app/static/javascript/sidebar.js
@@ -1,17 +1,36 @@
-document.getElementById('admin-item').addEventListener('click', function () {
-    toggleSubMenu('admin-submenu');
-  });
-  
-  document.getElementById('developers-item').addEventListener('click', function () {
-    toggleSubMenu('developers-submenu');
-  });
-  
-  function toggleSubMenu(subMenuId) {
-    var subMenu = document.getElementById(subMenuId);
-    if (subMenu.style.display === 'block') {
-      subMenu.style.display = 'none';
+const adminMainMenu = document.getElementById("admin-main-menu");
+const expandImage = document.getElementById("expand-img");
+const adminLinks = document.querySelectorAll(".admin-container a");
+const sidebar = document.querySelector(".sidebar");
+
+// Track the toggle state
+let isToggled = false;
+
+// Function to reset the image rotation to 0 degrees
+function resetImage() {
+  expandImage.style.transform = "rotate(0deg)";
+}
+
+// Click event handler for the admin main menu
+adminMainMenu.addEventListener("click", function () {
+  // Toggle the visibility of admin links and rotate the image
+  adminLinks.forEach((link) => {
+    if (link.style.display === "block" || link.style.display === "") {
+      link.style.display = "none";
+      resetImage(); // Reset the image rotation to 0 degrees
     } else {
-      subMenu.style.display = 'block';
+      link.style.display = "block";
+      expandImage.style.transform = "rotate(270deg)";
     }
+  });
+});
+
+// Hide admin links and reset the image when the mouse leaves the sidebar
+sidebar.addEventListener("mouseleave", function () {
+  if (!isToggled) {
+    adminLinks.forEach((link) => {
+      link.style.display = "none";
+      resetImage();
+    });
   }
-  
+});
