6170516e2c4846e2f589c15ef6ca3d6a818c28d8
jorgie04 <jorgeedralev@gmail.com>
2023-10-29 21:13:13 +0800

    data-comparison applied in analytics

--- app/static/javascript/analytics.js
+++ app/static/javascript/analytics.js
@@ -1,6 +1,6 @@
 let chartGenerated = false;
 let chart;
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('myChart1').getContext('2d');
 
 document.addEventListener('DOMContentLoaded', function() {
     fetchAndGenerateChart();
@@ -21,42 +21,58 @@ function fetchAndGenerateChart() {
     const selectedFirstMetric = document.getElementById('firstMetricDropdown').value;
     const selectedSecondMetric = document.getElementById('secondMetricDropdown').value;
 
-    fetch(`/get_data/${selectedFirstMetric}/${selectedSecondMetric}`)
+    fetch(`/get_data_past/${selectedFirstMetric}/${selectedSecondMetric}`)
         .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            generateBarGraph(data, selectedFirstMetric, selectedSecondMetric);
+        .then(data1 => {
+            // Fetch data for the second dataset
+            fetch(`/get_data_new/${selectedFirstMetric}/${selectedSecondMetric}`)
+                .then(response => response.json())
+                .then(data2 => {
+                    generateBarGraph(data1, data2, selectedFirstMetric, selectedSecondMetric);
+                });
         });
+    
 }
 
-function generateBarGraph(data, selectedFirstMetric, selectedSecondMetric) {
+function generateBarGraph(data1, data2, selectedFirstMetric, selectedSecondMetric) {
     if (chart) {
         chart.destroy();
     }
 
-    var labels = data.map(item => item[selectedFirstMetric]);
-    var values = data.map(item => item[selectedSecondMetric]);
+    var labels = data1.map(item => item[selectedFirstMetric]);
+    var values1 = data1.map(item => item[selectedSecondMetric]);
+    var values2 = data2.map(item => item[selectedSecondMetric]);
 
     chart = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: labels,
             datasets: [{
-                label: selectedFirstMetric,
-                data: values,
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
+                label: 'Past Data',
+                data: values1,
+                backgroundColor: 'rgba(9, 83, 113, 1)' 
+            }, {
+                label: 'Latest Data',
+                data: values2,
+                backgroundColor: 'rgba(160, 216, 224, 1)' 
             }]
         },
         options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    display: false
+                },
+            },
             scales: {
                 y: {
                     beginAtZero: true
                 }
             }
         }
-    });
+    })
 }
+
 if (window.history.replaceState) {
     window.history.replaceState(null, null, window.location.href);
 }
