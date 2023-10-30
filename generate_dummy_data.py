3cb75f67e692a0dcf622d77a76d07efd459c37df
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2023-10-30 21:25:25 +0800

    Dummy data input separation

--- generate_dummy_data.py
+++ generate_dummy_data.py
@@ -5,44 +5,7 @@ from datetime import date, timedelta
 from faker import Faker
 from app import db, app
 from app.models import StudentInformation, PersonalInformation, FamilyBackground, HealthInformation, EducationalBackground, PsychologicalAssessments
-
-course_names = [
-    "Bachelor of Science in Mechanical Engineering",
-    "Bachelor of Science in Architecture",
-    "Bachelor of Science in Civil Engineering",
-    "Bachelor of Science in Electrical Engineering",
-    "Bachelor of Science in Electronics Engineering",
-    "Bachelor of Science in Computer Engineering",
-    "Bachelor of Science in Industrial Engineering (Boni Campus)",
-    "Bachelor of Science in Information Technology (Boni Campus)",
-    "Bachelor of Science in Instrumentation and Control Engineering (Boni Campus)",
-    "Bachelor of Science in Mechatronics",
-    "Bachelor of Science in Accountancy",
-    "Bachelor of Science in Entrepreneurship",
-    "Bachelor of Science in Office Administration",
-    "Bachelor of Science in Business Administration major in Operations Management",
-    "Bachelor of Science in Business Administration major in Marketing Management",
-    "Bachelor of Science in Business Administration major in Financial Management",
-    "Bachelor of Science in Business Administration major in Human Resource Management",
-    "Bachelor of Secondary Education major in English",
-    "Bachelor of Secondary Education major in Math",
-    "Bachelor of Secondary Education major in Science (Boni Campus)",
-    "Bachelor of Secondary Education major in Social Studies",
-    "Bachelor of Secondary Education Major in Filipino",
-    "Bachelor of Technical-Vocational Teacher Education major in Animation",
-    "Bachelor of Technical-Vocational Teacher Education major in Computer Hardware Servicing",
-    "Bachelor of Technical-Vocational Teacher Education major in Visual Graphic Design",
-    "Bachelor or Technical-Vocational Teacher Education Major in Garments Fashion and Design",
-    "Bachelor or Technical-Vocational Teacher Education Major in Electronics Technology",
-    "Bachelor or Technical-Vocational Teacher Education Major in Welding and Fabrications Technology",
-    "Bachelor of Science in Psychology",
-    "Bachelor of Arts in Political Science",
-    "Bachelor of Science in Statistics (Boni Campus)",
-    "Bachelor of Science in Biology (Boni Campus)",
-    "Bachelor of Science in Astronomy",
-    "Institute of Human Kinetics",
-    "Bachelor of Science in Physical Education (Boni Campus)"
-]
+from dummy_data_input import course_names
 
 
 with app.app_context():
