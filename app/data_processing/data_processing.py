8e60525873d622d6f5ee4d4560ec6f3eb060ef82
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2023-10-31 06:45:36 +0800

    Fixed colleges

--- app/data_processing/data_processing.py
+++ app/data_processing/data_processing.py
@@ -4,49 +4,52 @@ import plotly.express as px
 
 from app import app, db
 from app.models.models import *
-from sqlalchemy.orm import joinedload
-
 from flask import jsonify
-
-
-with app.app_context():
-    data = (
-        StudentInformation.query
-        .options(joinedload(StudentInformation.personal_information))
-        .options(joinedload(StudentInformation.family_background))
-        .options(joinedload(StudentInformation.health_information))
-        .options(joinedload(StudentInformation.educational_background))
-        .options(joinedload(StudentInformation.psychological_assessments))
-        .all()
-    )
+from sqlalchemy.orm import joinedload
 
 
 def process_data(first_metric, second_metric):
-    data_list = []
-    for record in data:
-        personal_information = record.personal_information
-        family_background = record.family_background
-        health_information = record.health_information
-        educational_background = record.educational_background
-        psychological_assessments = record.psychological_assessments
+    with app.app_context():
+        data = (
+            StudentInformation.query
+            .options(joinedload(StudentInformation.personal_information))
+            .options(joinedload(StudentInformation.family_background))
+            .options(joinedload(StudentInformation.health_information))
+            .options(joinedload(StudentInformation.educational_background))
+            .options(joinedload(StudentInformation.psychological_assessments))
+            .all()
+        )
+        count = 0
+        data_list = []
+        for record in data:
+            personal_information = record.personal_information
+            family_background = record.family_background
+            health_information = record.health_information
+            educational_background = record.educational_background
+            psychological_assessments = record.psychological_assessments
 
-        data_list.append({
-            'student_id': record.student_id,
-            'course': record.course,
-            'campus': record.campus,
-            'gpa': record.gpa,
-            'year_level': record.year_level,
-            'age': personal_information.age,
-            'gender': personal_information.gender,
-            'religion': personal_information.religion,
-            'nationality': personal_information.nationality,
-            'learning_styles': psychological_assessments.learning_styles,
-            'personality_test': psychological_assessments.personality_test,
-            'iq_test': psychological_assessments.iq_test
-        })
+            course_name = record.course
+            college = Course.query.filter_by(name=course_name).first()
+            college_name = college.college.name if college else None 
 
-    df = pd.DataFrame(data_list)
-    data_mean = df.groupby(first_metric)[second_metric].mean().reset_index()
-    data_dict = data_mean.to_dict(orient='records')
+            data_list.append({
+                'student_id': record.student_id,
+                'course': course_name,
+                'campus': record.campus,
+                'gpa': record.gpa,
+                'college': college_name,
+                'year_level': record.year_level,
+                'age': personal_information.age,
+                'gender': personal_information.gender,
+                'religion': personal_information.religion,
+                'nationality': personal_information.nationality,
+                'learning_styles': psychological_assessments.learning_styles,
+                'personality_test': psychological_assessments.personality_test,
+                'iq_test': psychological_assessments.iq_test
+            })
 
-    return data_dict
+        df = pd.DataFrame(data_list)
+        data_mean = df.groupby(first_metric)[second_metric].mean().reset_index()
+        data_dict = data_mean.to_dict(orient='records')
+
+        return data_dict
