b5f7cbcf23837fb21d10888c5167365b22e73182
Justin Marley Fontanilla <71302306+jfontz@users.noreply.github.com>
2023-11-01 01:02:52 +0800

    Fixed: minor bug, Added icon animation

--- app/static/javascript/sidebar.js
+++ app/static/javascript/sidebar.js
@@ -11,6 +11,12 @@ function resetImage() {
   expandImage.style.transform = "rotate(0deg)";
 }
 
+// Hide admin links and reset the image when the page loads
+adminLinks.forEach((link) => {
+  link.style.display = "none";
+  resetImage();
+});
+
 // Click event handler for the admin main menu
 adminMainMenu.addEventListener("click", function () {
   // Toggle the visibility of admin links and rotate the image
@@ -18,19 +24,23 @@ adminMainMenu.addEventListener("click", function () {
     if (link.style.display === "block" || link.style.display === "") {
       link.style.display = "none";
       resetImage(); // Reset the image rotation to 0 degrees
+      isToggled = false;
     } else {
       link.style.display = "block";
-      expandImage.style.transform = "rotate(270deg)";
+      expandImage.style.transform = "rotate(-90deg)";
+      isToggled = true;
     }
   });
 });
 
 // Hide admin links and reset the image when the mouse leaves the sidebar
 sidebar.addEventListener("mouseleave", function () {
-  if (!isToggled) {
+  if (isToggled) {
+    // Update the flag only if it's currently toggled open
     adminLinks.forEach((link) => {
       link.style.display = "none";
       resetImage();
     });
+    isToggled = false; // Update the flag to reflect the change
   }
 });
