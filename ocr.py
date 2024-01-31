9474c05f43d288f93aff807c5aa7bb77ccc2ecb9
jorgie04 <jorgeedralev@gmail.com>
2024-01-31 17:50:19 +0800

    ocr-testing

--- ocr.py
+++ ocr.py
@@ -1,9 +1,7 @@
-from PIL import Image
+# from PIL import Image
 
-import pytesseract
+# import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\LENOVO\Documents\GitHub\MindSight\Tesseract-OCR\tesseract.exe'
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Users\LENOVO\Documents\GitHub\MindSight\Tesseract-OCR\tesseract.exe'
 
-text = pytesseract.image_to_string(Image.open('page 1.jpg'), lang='eng+fil')
-print(text)
-
+# text = pytesseract.image_to_string(Image.open('page 1.jpg'), lang='eng+fil')
