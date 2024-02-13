1997c6b0ca1ade419c063e9752171a15755493eb
jorgie04 <jorgeedralev@gmail.com>
2024-02-13 23:25:54 +0800

    added pages for dashboard breakdown

--- ocr.py
+++ ocr.py
@@ -1,7 +1,7 @@
-# from PIL import Image
+from PIL import Image
+import pytesseract
 
-# import pytesseract
 
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Users\LENOVO\Documents\GitHub\MindSight\Tesseract-OCR\tesseract.exe'
-
-# text = pytesseract.image_to_string(Image.open('page 1.jpg'), lang='eng+fil')
+class OCR():
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Users\LENOVO\Documents\GitHub\MindSight\Tesseract-OCR\tesseract.exe'
+    text = pytesseract.image_to_string(Image.open(''), lang='eng+fil')
