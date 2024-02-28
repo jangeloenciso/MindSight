7a5ef1e2ddd1a4664b03024753e56fc86f61987d
jorgie04 <jorgeedralev@gmail.com>
2024-02-28 19:50:33 +0800

    added pop-up confirmation before log out

---
+++ app/static/javascript/logout_confirmation.js
@@ -0,0 +1,17 @@
+function confirmLogout() {
+    swal.fire({
+        title: 'Are you sure?',
+        text: "You're about to log out.",
+        iconHtml: '<img class="custom-icon" src="static/question.png">',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, log out.',
+        customClass: {
+          confirmButton: `confirm-button-class`,
+          cancelButton: 'cancel-button-class'
+        }
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = "logout";
+          }
+        });
+}
