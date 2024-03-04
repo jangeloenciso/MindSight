9995b6ba57c2399892dd48b31825477d81d9db39
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2024-03-04 12:56:45 +0800

    Models fix

--- app/models/models.py
+++ app/models/models.py
@@ -369,12 +369,20 @@ class LegalHistory(db.Model):
     on_probation = db.Column(db.Boolean, default=False)
     has_been_arrested = db.Column(db.Boolean, default=False)
 
+    student_id = db.Column(db.String(20), db.ForeignKey('basic_information.student_id'))
+
+class Conviction(db.Model):
+    __tablename__ = 'convictions'
+
+    id = db.Column(db.Integer, primary_key=True)
+
     # TODO: THIS WAS LAZY. FOR TESTING PURPOSES ONLY. CREATE SEPARATE MODEL/TABLE LATER
     conviction = db.Column(db.String(50))
-    conviction_date = db.Date
+    conviction_date = db.Column(db.Date())
     conviction_outcome = db.Column(db.String(50))
 
-    student_id = db.Column(db.String(20), db.ForeignKey('basic_information.student_id'))
+    legal_history_id = db.Column(db.Integer, db.ForeignKey('legal_history.id'), nullable=False)
+
 
 class AdditionalInformation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
