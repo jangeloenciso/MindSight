e35e698b31466164a4ec7c841a98fb55b3ecce7e
Justin Marley Fontanilla <71302306+jfontz@users.noreply.github.com>
2024-03-06 19:42:47 +0800

    cleanup: deleted OCR-related files

--- ocr.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import pytesseract
-
-
-class OCR():
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Users\LENOVO\Documents\GitHub\MindSight\Tesseract-OCR\tesseract.exe'
-    text = pytesseract.image_to_string(Image.open(''), lang='eng+fil')
