b348a20f6a93bf0a4c71c70a7c31377ac3381eca
jorgie04 <jorgeedralev@gmail.com>
2024-03-07 18:31:26 +0800

    added info to swal for data privacy

--- app/static/javascript/add_confirmation.js
+++ app/static/javascript/add_confirmation.js
@@ -2,11 +2,19 @@ document.getElementById('toggleConfirmation').addEventListener('click', function
     swal.fire({
         title: "Data Privacy Act of 2012",
         html: `
-        <div class="data-privacy">
-            Before answering the form, please confirm that you agree to our adherence to the
-            <a href="/static/privacy_gov_ph_data-privacy-act.pdf" target="_blank"
-            >Data Privacy Act of 2012.</a
-            >
+        <div class="modal">
+            <p>
+                This form will be handled with the utmost confidentiality <br>
+                and in strict accordance with the <a href="/static/privacy_gov_ph_data-privacy-act.pdf" target="_blank"
+                ><b>Data Privacy Act, <br>Republic Act 10173.</b> </a>
+            </p>
+            <p>
+                I hereby confirm that I am aware of this and authorize <br>
+                the <b>Guidance and Counseling Services Center </b>to collect <br>
+                and use the information on this form for counseling. This <br>
+                information will significantly contribute to the success of <br>
+                this important endeavor.
+            </p>
         </div>
         `,
         showCancelButton: true,
