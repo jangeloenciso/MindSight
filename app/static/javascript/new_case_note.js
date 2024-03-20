b6c2eea885718acfbd59e6c33fa915a5e3a2b837
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2024-03-20 17:07:35 +0800

    Case note bug fix

--- app/static/javascript/new_case_note.js
+++ app/static/javascript/new_case_note.js
@@ -1,42 +1,40 @@
 function newCaseNote() {
-  var caseNoteContainer = document.getElementById("caseNoteContainer");
-  var newCaseNote = caseNoteContainer.cloneNode(true);
+  var caseNoteContainers = document.querySelectorAll("#caseNoteContainer");
+  var lastCaseNoteContainer = caseNoteContainers[caseNoteContainers.length - 1];
+  var newCaseNote = lastCaseNoteContainer.cloneNode(true);
 
   // Clear input values in the cloned case note
   var inputs = newCaseNote.getElementsByTagName("input");
 
-  var elementsWithId = document.querySelectorAll("#" + "caseNoteContainer");
-
-  var last_id = elementsWithId.length - 1;
+  var last_id = caseNoteContainers.length;
 
   newId = last_id + 1;
   for (var i = 0; i < inputs.length; i++) {
-    inputs[i].value = "";
-    inputs[i].setAttribute("id", inputs[i].getAttribute("id") + newId);
-    inputs[i].setAttribute("name", inputs[i].getAttribute("name"));
+      inputs[i].value = "";
+      inputs[i].setAttribute("id", inputs[i].getAttribute("id") + newId);
+      inputs[i].setAttribute("name", inputs[i].getAttribute("name"));
   }
-// Create a div container for the delete button
-var deleteButtonContainer = document.createElement("div");
-deleteButtonContainer.classList.add("DelBtn-container");
+  // Create a div container for the delete button
+  var deleteButtonContainer = document.createElement("div");
+  deleteButtonContainer.classList.add("DelBtn-container");
 
-// Create a delete button for the new case note
-var deleteButton = document.createElement("button");
-deleteButton.textContent = "Delete";
-deleteButton.classList.add("DelBtn");
-deleteButton.classList.add("Btn");
+  // Create a delete button for the new case note
+  var deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.classList.add("DelBtn");
+  deleteButton.classList.add("Btn");
 
-deleteButton.onclick = function () {
-    // Remove the corresponding case note container
-    newCaseNote.remove();
-};
+  deleteButton.onclick = function () {
+      // Remove the corresponding case note container
+      newCaseNote.remove();
+  };
 
-// Append the delete button to the container
-deleteButtonContainer.appendChild(deleteButton);
+  // Append the delete button to the container
+  deleteButtonContainer.appendChild(deleteButton);
 
-// Append the delete button container to the new case note
-newCaseNote.appendChild(deleteButtonContainer);
+  // Append the delete button container to the new case note
+  newCaseNote.appendChild(deleteButtonContainer);
 
-// Append the cloned case note to the container
-caseNoteContainer.parentNode.appendChild(newCaseNote);
-
+  // Append the cloned case note after the last case note container
+  lastCaseNoteContainer.parentNode.appendChild(newCaseNote);
 }
