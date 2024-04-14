ad6ad04acd9c3d5851403161b61d8780abe0fb1e
Justin Marley Fontanilla <71302306+jfontz@users.noreply.github.com>
2024-04-15 02:08:06 +0800

    added loading animation

--- app/static/javascript/dashboard.js
+++ app/static/javascript/dashboard.js
@@ -1,113 +1,146 @@
 let chart1;
+// console.log("loading charts");
 
-document.addEventListener('DOMContentLoaded', function() {
-    fetchAndGenerateChart(1);
+document.addEventListener("DOMContentLoaded", function () {
+  fetchAndGenerateChart(1);
 });
 
-function fetchAndGenerateChart(chartNumber) {
-    const dataEndpoint = '/get_data/college';
-
-    fetch(dataEndpoint)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            generateLineGraph(data, chartNumber);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+function showLoadingAnimation(containerId) {
+  const container = document.getElementById(containerId);
+  const spinner = document.createElement("img");
+  spinner.src = "/static/loader.gif"; // URL of loader image
+  spinner.className = "spinner";
+  container.appendChild(spinner);
 }
 
-function generateLineGraph(data, chartNumber) { // Change function name to generateLineGraph
-    const chartContainer = `myChart${chartNumber}`;
-    const ctx = document.getElementById(chartContainer).getContext('2d');
+function hideLoadingAnimation(containerId) {
+  const container = document.getElementById(containerId);
+  const spinner = container.querySelector(".spinner");
+  if (spinner) {
+    container.removeChild(spinner);
+  }
+}
 
-    const collegeNames = Object.keys(data);
-    const activeCounts = collegeNames.map(college => data[college]['Active'][college]);
-    const inactiveCounts = collegeNames.map(college => data[college]['Inactive'][college]);
-    const terminatedCounts = collegeNames.map(college => data[college]['Terminated'][college]);
-    
-    console.log('Active', activeCounts);
-    console.log('Inactive', inactiveCounts);
-    console.log('Terminated', terminatedCounts);
-    
-    if (chartNumber === 1) {
-        if (chart1) {
-            chart1.destroy();
-        }
+function fetchAndGenerateChart(chartNumber) {
+  const dataEndpoint = "/get_data/college";
+  const chartContainerId = `myChart${chartNumber}`;
 
-        chart1 = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: collegeNames,
-                datasets: [{
-                    label: 'Active',
-                    data: activeCounts,
-                    backgroundColor: 'rgba(0, 128, 0, 0.3)',
-                    borderColor: 'rgba(0, 128, 0, 1)',
-                    borderWidth: 2,
-                    pointBackgroundColor: 'rgba(0, 128, 0, 0.3)',
-                    pointBorderWidth: 0.3,
-                    tension: 0.3
-                }, {
-                    label: 'Inactive',
-                    data: inactiveCounts,
-                    backgroundColor: 'rgba(86, 86, 86, 0.3)',
-                    borderColor: 'rgba(86, 86, 86, 1)',
-                    borderWidth: 2,
-                    pointBackgroundColor: 'rgba(157, 169, 158, 0.3)',
-                    pointBorderWidth: 0.3,
-                    tension: 0.3
-                }, {
-                    label: 'Terminated',
-                    data: terminatedCounts,
-                    backgroundColor: 'rgba(255, 0, 0, 0.3)',
-                    borderColor: 'rgba(255, 0, 0, 1)',
-                    borderWidth: 2,
-                    pointBackgroundColor: 'rgba(255, 0, 0, 0.3)',
-                    pointBorderWidth: 0.3,
-                    tension: 0.3
-                }]
-            },
-            options: {
-                responsive: true,
-                scales: {
-                    x: {
-                        ticks: {
-                            color: 'rgba(9, 83, 113, 1)'
-                        },
-                        grid: {
-                            color: 'rgba(190, 205, 211, 1)'
-                        }
-                    },
-                    y: {
-                        beginAtZero: true,
-                        ticks: {
-                            color: 'rgba(219, 147, 84, 1)'
-                        },
-                        grid: {
-                            color: 'rgba(190, 205, 211, 1)'
-                        }
-                    }
-                },
-                // animations: {
-                //     tension: {
-                //       duration: 1000,
-                //       easing: 'easeInBounce',
-                //       from: 1,
-                //       to: 0,
-                //       loop: true
-                //     }
-                //   },
-            }
-        });
+  // Show loading animation
+  showLoadingAnimation("total-cases");
+
+  fetch(dataEndpoint)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      generateLineGraph(data, chartNumber, chartContainerId);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
+
+function generateLineGraph(data, chartNumber, chartContainerId) {
+  const container = document.getElementById(chartContainerId);
+  const ctx = container.getContext("2d");
+
+  const collegeNames = Object.keys(data);
+  const activeCounts = collegeNames.map(
+    (college) => data[college]["Active"][college]
+  );
+  const inactiveCounts = collegeNames.map(
+    (college) => data[college]["Inactive"][college]
+  );
+  const terminatedCounts = collegeNames.map(
+    (college) => data[college]["Terminated"][college]
+  );
+
+  if (chartNumber === 1) {
+    if (chart1) {
+      chart1.destroy();
     }
+
+    try {
+      chart1 = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: collegeNames,
+          datasets: [
+            {
+              label: "Active",
+              data: activeCounts,
+              backgroundColor: "rgba(0, 128, 0, 0.2)",
+              borderColor: "rgba(0, 128, 0, 1)",
+              borderWidth: 2,
+              pointBackgroundColor: "rgba(0, 128, 0, 0.3)",
+              pointBorderWidth: 0.3,
+              tension: 0.3,
+            },
+            {
+              label: "Inactive",
+              data: inactiveCounts,
+              backgroundColor: "rgba(86, 86, 86, 0.2)",
+              borderColor: "rgba(86, 86, 86, 1)",
+              borderWidth: 2,
+              pointBackgroundColor: "rgba(157, 169, 158, 0.3)",
+              pointBorderWidth: 0.3,
+              tension: 0.3,
+            },
+            {
+              label: "Terminated",
+              data: terminatedCounts,
+              backgroundColor: "rgba(255, 0, 0, 0.2)",
+              borderColor: "rgba(255, 0, 0, 1)",
+              borderWidth: 2,
+              pointBackgroundColor: "rgba(255, 0, 0, 0.3)",
+              pointBorderWidth: 0.3,
+              tension: 0.3,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              ticks: {
+                color: "rgba(9, 83, 113, 1)",
+              },
+              grid: {
+                color: "rgba(190, 205, 211, 1)",
+              },
+            },
+            y: {
+              beginAtZero: true,
+              ticks: {
+                color: "rgba(219, 147, 84, 1)",
+              },
+              grid: {
+                color: "rgba(190, 205, 211, 1)",
+              },
+            },
+          },
+          plugins: {
+            tooltip: {
+              mode: "index",
+            },
+          },
+        },
+      });
+
+      // Call success callback if chart is successfully loaded
+      if (chart1) {
+        // console.log("Chart successfully loaded");
+        hideLoadingAnimation("total-cases");
+      }
+    } catch (error) {
+      console.error("Error creating chart:", error);
+    }
+  }
 }
 
 if (window.history.replaceState) {
-    window.history.replaceState(null, null, window.location.href);
+  window.history.replaceState(null, null, window.location.href);
 }
