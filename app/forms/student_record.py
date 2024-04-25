a7a7e4d9dfb1a993a4f7762b966acb15712e7881
jorgie04 <jorgeedralev@gmail.com>
2024-04-25 20:36:54 +0800

    last

--- app/forms/student_record.py
+++ app/forms/student_record.py
@@ -234,7 +234,7 @@ class StudentRecordForm(FlaskForm):
     nature_of_concern = StringField('Nature of Concern', validators=[Optional()])
     counselor = StringField('Counselor', validators=[Optional()])
     personal_agreement = BooleanField(validators=[DataRequired()])
-    personal_agreement_date = DateTimeLocalField(validators=[DataRequired()], default=datetime.now(timezone('Asia/Manila')))
+    personal_agreement_date = DateTimeLocalField(validators=[DataRequired()], default=datetime.now(timezone('Asia/Kuala_Lumpur')))
     
     status = StringField('Status', validators=[Optional()])
     remarks = StringField('Remarks', validators=[Optional()])
